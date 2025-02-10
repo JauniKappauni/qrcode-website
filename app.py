d6446c342945c2d99d112a70aeee49544c5013ed
JauniKappauni <social.jauni@gmail.com>
2025-02-10 18:42:00 +0100

    Imported request variable and send_file function Added GET and POST method to index path Declared qrc and qrcpath to be an empty string to prevent "UnboundLocalError" Added if statement to only proceed on post method to prevent auto run of code Added retrieval of input data from html form Created QR Code from html form data and assigned it to qrc variable Added assignment to qrcpath variable Added saving of created qrcode to qrcpath variable value Added return of qr code

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
 import qrcode
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def abc():
-    return render_template("index.html")
+    qrc = ""
+    qrcpath = ""
+    if request.method == "POST":
+        data = request.form["data"]
+        qrc = qrcode.make(data)
+        
+        qrcpath = "static/qrcode.png"
+        qrc.save(qrcpath)
+
+    return render_template("index.html", qrc=qrcpath)
 
 if __name__ == "__main__":
     app.run(debug=True)
